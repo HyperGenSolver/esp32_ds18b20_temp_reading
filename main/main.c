a37220f53555f4fab81c2b2a617335f48a4fe5a8
Cedric Weibel <cedric.weibel@gmail.com>
2024-10-27 22:05:41 +0100

    init

---
+++ main/main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include "esp_log.h"
+#include "owb.h"
+
+// https://github.com/DavidAntliff/esp32-ds18b20-example/blob/master/main/app_main.c
+void app_main(void)
+{
+    ESP_LOGI("main", "Hello world!");
+}
